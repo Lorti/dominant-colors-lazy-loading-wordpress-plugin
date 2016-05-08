52be950d9eb0afe3df848d4277f3e128ea935dea
Manuel Wieser <office@manuelwieser.com>
2016-05-08 13:25:32 +0200

    added another test for post meta creation

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -3,18 +3,36 @@
 class AdminTest extends WP_UnitTestCase {
 
 	protected $admin;
+	protected $image;
 
 	public function setUp() {
 		parent::setUp();
 		$plugin      = new Dominant_Colors_Lazy_Loading();
 		$this->admin = new Dominant_Colors_Lazy_Loading_Admin( $plugin->get_plugin_name(), $plugin->get_version() );
+		$this->image = realpath( dirname( __FILE__ ) ) . '/test.jpg';
 	}
 
 	function test_calculate_dominant_color() {
 
-		$path     = realpath( dirname( __FILE__ ) ) . '/test.jpg';
 		$expected = 'ca7a7b';
-		$actual   = $this->admin->calculate_dominant_color( $path );
+		$actual   = $this->admin->calculate_dominant_color( $this->image );
+		$this->assertEquals( $expected, $actual );
+
+	}
+
+	function test_add_dominant_color_post_meta() {
+
+		$id = $this->factory->attachment->create_object( $this->image, 0, array(
+			'post_mime_type' => 'image/jpeg',
+			'post_type'      => 'attachment'
+		) );
+
+		$expected = 'ca7a7b';
+
+		$actual = $this->admin->add_dominant_color_post_meta( $id );
+		$this->assertEquals( $expected, $actual );
+
+		$actual = get_post_meta( $id, 'dominant_color', true );
 		$this->assertEquals( $expected, $actual );
 
 	}
